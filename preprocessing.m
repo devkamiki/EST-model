02e19d4b5fb76bf69090ea65dd86bf7215137c10
devkamiki <kamiki@disroot.org>
2026-04-20 20:10:24 +0200

    changed preprocessing.m

--- preprocessing.m
+++ preprocessing.m
@@ -5,13 +5,13 @@
 
 timeUnit   = 's';
 
-supplyFile = "SolarExample_supply.csv";
+supplyFile = "Team07_supply.csv";
 supplyUnit = "kW";
 
 % load the supply data
 Supply = loadSupplyData(supplyFile, timeUnit, supplyUnit);
 
-demandFile = "SolarExample_demand.csv";
+demandFile = "Team07_demand.csv";
 demandUnit = "kW";
 
 % load the demand data
